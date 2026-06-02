1293872c1a0c376b12af4425e2b97860a772cf7c
antoniomadureira <antoniomadureira@gmail.com>
2026-06-02 17:18:25 +0100

    Correção do caminho base do Vite para o Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/strava-lab/',
+  base: '/', // Volta a apontar para a raiz (ou podes simplesmente apagar esta linha)
 })
